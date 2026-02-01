a66b45f554bdc4d07cae9ef4fbe4c6148f81f407
hillary445 <hilaryprit02@gmail.com>
2026-02-01 22:59:25 +0300

    Fixed some the cartContext for the cart to display

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import AuthProvider from './context/AuthContext.jsx'
+import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
+      <CartProvider>
       <App />
+      </CartProvider>
     </AuthProvider>
   </BrowserRouter>
 )
